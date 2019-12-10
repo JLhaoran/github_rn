b997c24d0afda6fd79015dff9fa4bf3eb96b2796
Haoran Li <835067575@qq.com>
2019-12-10 10:07:57 +0800

    处理安卓物理返回键

--- js/page/HomePage.js
+++ js/page/HomePage.js
@@ -9,10 +9,39 @@ import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Entypo from 'react-native-vector-icons/Entypo';
 import NavigationUtil from '../navigators/NavigationUtil';
-
 import DynamicTabNavigator from  '../navigators/DynamicTabNavigator';
 
-export default class HomePage extends React.Component{
+import {NavigationActions} from "react-navigation";
+import {connect} from "react-redux";
+import BackPressComponent from "../common/BackPressComponent";
+
+ class HomePage extends React.Component{
+    constructor(props) {
+        super(props);
+        this.backPress = new BackPressComponent({backPress: this.onBackPress});
+    }
+
+    componentDidMount() {
+        this.backPress.componentDidMount();
+    }
+
+    componentWillUnmount() {
+        this.backPress.componentWillUnmount();
+    }
+    /**
+     * 处理 Android 中的物理返回键
+     * https://reactnavigation.org/docs/en/redux-integration.html#handling-the-hardware-back-button-in-android
+     * @returns {boolean}
+     */
+    onBackPress = () => {
+        const {dispatch, nav} = this.props;
+        //if (nav.index === 0) {
+        if (nav.routes[1].index === 0) {//如果RootNavigator中的MainNavigator的index为0，则不处理返回事件
+            return false;
+        }
+        dispatch(NavigationActions.back());
+        return true;
+    };
     // _tabNavigator(){
     //     const BottomTabNavigator = createBottomTabNavigator({
     //         PopularPage: {
@@ -76,5 +105,12 @@ export default class HomePage extends React.Component{
        return <DynamicTabNavigator/>
   }
 }
+
+const mapStateToProps = state => ({
+    nav: state.nav
+});
+
+//注意：connect只是个function，并不应定非要放在export后面
+export default connect(mapStateToProps)(HomePage);
 
 
